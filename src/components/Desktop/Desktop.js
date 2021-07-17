f7146a9a76e611501593f1b9c985ea8128f58ff8
Samuel Lambert <samuel.lambert@hotmail.com>
2021-07-17 19:15:35 -0400

    Enable dragging for multiple windows

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 import styles from "./Desktop.module.scss";
 
 export default function Deskop({ children }) {
@@ -7,49 +7,54 @@ export default function Deskop({ children }) {
   // This means a drag could stop prematurely if the user moves the mouse
   // too fast. By setting the event handlers on the Desktop component, we
   // can be sure not to miss any mouse event.
-  const windowState = useRef({ xOffset: 0, yOffset: 0 });
+  const [isActive, setIsActive] = useState(false);
+  const [activeElement, setActiveElement] = useState(null);
 
   const handleDragStart = (event) => {
     const element = event.target;
-
-    if (!element?.classList.contains("titleBar")) {
-      return;
-    }
-
-    const state = windowState.current;
-    state.element = element;
+    if (!element?.classList.contains("titleBar")) return;
+    if (!element.xOffset) element.xOffset = 0;
+    if (!element.yOffset) element.yOffset = 0;
 
     if (event.type === "touchstart") {
-      state.initialX = event.touches[0].clientX - state.xOffset;
-      state.initialY = event.touches[0].clientY - state.yOffset;
+      element.initialX = event.touches[0].clientX - element.xOffset;
+      element.initialY = event.touches[0].clientY - element.yOffset;
     } else {
-      state.initialX = event.clientX - state.xOffset;
-      state.initialY = event.clientY - state.yOffset;
+      element.initialX = event.clientX - element.xOffset;
+      element.initialY = event.clientY - element.yOffset;
     }
+
+    setIsActive(true);
+    setActiveElement(element);
   };
 
   const handleDragEnd = () => {
-    windowState.current.element = null;
+    if (activeElement) {
+      activeElement.initialX = activeElement.currentX;
+      activeElement.initialY = activeElement.currentY;
+    }
+
+    setIsActive(false);
+    setActiveElement(null);
   };
 
   const handleDragMove = (event) => {
-    if (windowState.current.element) {
+    if (isActive) {
       event.preventDefault();
-      const state = windowState.current;
 
       if (event.type === "touchmove") {
-        state.currentX = event.touches[0].clientX - state.initialX;
-        state.currentY = event.touches[0].clientY - state.initialY;
+        activeElement.currentX = event.touches[0].clientX - activeElement.initialX;
+        activeElement.currentY = event.touches[0].clientY - activeElement.initialY;
       } else {
-        state.currentX = event.clientX - state.initialX;
-        state.currentY = event.clientY - state.initialY;
+        activeElement.currentX = event.clientX - activeElement.initialX;
+        activeElement.currentY = event.clientY - activeElement.initialY;
       }
 
-      state.xOffset = state.currentX;
-      state.yOffset = state.currentY;
+      activeElement.xOffset = activeElement.currentX;
+      activeElement.yOffset = activeElement.currentY;
 
-      // Using translate3d() to force GPU to draw animation.
-      state.element.parentNode.style.transform = `translate3d(${state.currentX}px, ${state.currentY}px, 0px)`;
+      const windowElement = activeElement.parentNode;
+      windowElement.style.transform = `translate3d(${activeElement.currentX}px, ${activeElement.currentY}px, 0px)`;
     }
   };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ export default function Index() {
   return (
     <Desktop>
       <Window title="Text Editor" width={640} height={480} top={10} left={10} />
+      <Window title="Browser" width={640} height={480} top={600} left={600} />
     </Desktop>
   );
 }
