7cee3a1b9e183dff178bd6447df8ad9684ec5263
Samuel Lambert <samuel.lambert@hotmail.com>
2021-08-16 21:08:58 -0400

    Add the isResizable property to Window

--- src/components/Window/Window.js
+++ src/components/Window/Window.js
@@ -3,17 +3,21 @@ import MaximizeIcon from "../Icons/Maximize/MaximizeIcon";
 import MinimizeIcon from "../Icons/Minimize/MinimizeIcon";
 import styles from "./Window.module.scss";
 
-export default function Window({ title, width, height, top, left, children }) {
+export default function Window({ title, width, height, top, left, children, isResizable = true }) {
   return (
     <div className={`window ${styles.window}`} style={{ width, height, top, left }}>
-      <div className={`topResizer ${styles.topResizer}`}></div>
-      <div className={`rightResizer ${styles.rightResizer}`}></div>
-      <div className={`bottomResizer ${styles.bottomResizer}`}></div>
-      <div className={`leftResizer ${styles.leftResizer}`}></div>
-      <div className={`topLeftResizer ${styles.topLeftResizer}`}></div>
-      <div className={`topRightResizer ${styles.topRightResizer}`}></div>
-      <div className={`bottomLeftResizer ${styles.bottomLeftResizer}`}></div>
-      <div className={`bottomRightResizer ${styles.bottomRightResizer}`}></div>
+      {isResizable && (
+        <>
+          <div className={`topResizer ${styles.topResizer}`}></div>
+          <div className={`rightResizer ${styles.rightResizer}`}></div>
+          <div className={`bottomResizer ${styles.bottomResizer}`}></div>
+          <div className={`leftResizer ${styles.leftResizer}`}></div>
+          <div className={`topLeftResizer ${styles.topLeftResizer}`}></div>
+          <div className={`topRightResizer ${styles.topRightResizer}`}></div>
+          <div className={`bottomLeftResizer ${styles.bottomLeftResizer}`}></div>
+          <div className={`bottomRightResizer ${styles.bottomRightResizer}`}></div>
+        </>
+      )}
       <div className={`titleBar ${styles.titleBar}`}>
         <div className={styles.title}>{title}</div>
         <MinimizeIcon />
