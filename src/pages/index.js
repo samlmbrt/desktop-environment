0ab3347cb68d8bc1e2e51898455704d3562b86ce
Samuel Lambert <samuel.lambert@hotmail.com>
2021-10-10 17:13:17 -0400

    Add another window for demonstration purposes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ import Window from "../components/Window/Window";
 export default function Index() {
   return (
     <Desktop>
-      <Window title="Text Editor" width={640} height={480} top={10} left={10} />
-      <Window title="Browser" width={640} height={480} top={600} left={600} />
+      <Window title="Window 1" width={640} height={480} top={10} left={10} />
+      <Window title="Window 2" width={640} height={480} top={110} left={110} />
+      <Window title="Window 3" width={640} height={480} top={210} left={210} />
     </Desktop>
   );
 }
