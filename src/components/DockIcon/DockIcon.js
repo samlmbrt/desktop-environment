67f6cfdf5d052f45a2ecc8173af1833e90e9e173
Samuel Lambert <samuel.lambert@hotmail.com>
2022-01-31 20:53:18 -0500

    Add a tooltip when hovering dock icons

--- src/components/DockIcon/DockIcon.js
+++ src/components/DockIcon/DockIcon.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import Image from "next/image";
 
 import styles from "./DockIcon.module.scss";
 
-const DockIcon = ({ icon, alt }) => {
+const DockIcon = ({ icon, alt, tooltip }) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div className={styles.dockIcon}>
-      <Image src={icon} alt={alt} placeholder="blur" width={64} height={64} />
+      <div className={`${styles.toolTip} ${isHovered && styles.hovered}`}>{tooltip}</div>
+      <Image
+        src={icon}
+        alt={alt}
+        placeholder="blur"
+        width={64}
+        height={64}
+        onMouseEnter={() => {
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false);
+        }}
+      />
       <div className={styles.dot} />
     </div>
   );
