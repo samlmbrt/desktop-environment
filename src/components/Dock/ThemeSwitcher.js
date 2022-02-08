84765a752833456c747da4e60cb381ff9bfe743d
Samuel Lambert <samuel.lambert@hotmail.com>
2022-02-07 21:09:49 -0500

    Add a moon and sun pictures in the theme switcher to make it look prettier

--- src/components/Dock/ThemeSwitcher.js
+++ src/components/Dock/ThemeSwitcher.js
@@ -1,4 +1,8 @@
+import Image from "next/image";
 import useThemeToggle from "/src/hooks/useThemeToggle";
+
+import moonIcon from "/public/icons/moon.png";
+import sunIcon from "/public/icons/sun.png";
 
 import styles from "./ThemeSwitcher.module.css";
 
@@ -7,6 +11,8 @@ const ThemeSwitcher = () => {
 
   return (
     <div className={styles.themeSwitcher} onClick={toggleTheme}>
+      <Image className={styles.moon} src={moonIcon} alt="Moon icon" width={24} height={24} />
+      <Image className={styles.sun} src={sunIcon} alt="Sun icon" width={24} height={24} />
       <div className={`${styles.switch} ${isDarkTheme && styles.enabled}`} />
     </div>
   );
