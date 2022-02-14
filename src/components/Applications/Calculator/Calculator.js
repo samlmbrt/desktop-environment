cb1c3ff3c6afcb81749e61248df2954a1caaaba4
Samuel Lambert <samuel.lambert@hotmail.com>
2022-02-14 17:35:03 -0500

    Add calculator logic

--- src/components/Applications/Calculator/Calculator.js
+++ src/components/Applications/Calculator/Calculator.js
@@ -1,31 +1,109 @@
+import { useState } from "react";
 import Window from "/src/components/Window/Window";
 
 import styles from "./Calculator.module.css";
 
+const sanitizeOperationString = (str) => {
+  return str.replace("÷", "/").replace("×", "*");
+};
+
 const Calculator = (props) => {
+  const [tokens, setTokens] = useState([]);
+  const [hasError, setHasError] = useState(false);
+
+  const ArithmeticButton = ({ className, children }) => {
+    return (
+      <div
+        className={className}
+        onClick={() => {
+          if (tokens.length === 0 && children === "0") return;
+          setTokens([...tokens, children]);
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  const FunctionButton = ({ className, callback, children }) => {
+    return (
+      <div
+        className={className}
+        onClick={() => {
+          if (typeof callback === "function") {
+            callback();
+          }
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+
   return (
     <Window {...props} title="Calculator" isResizable={false}>
       <div className={styles.calculator} tabIndex={-1}>
-        <div className={`${styles.screen}`}>123123123123123</div>
-        <div className={`${styles.button} ${styles.ac} ${styles.lightGrey}`}>AC</div>
-        <div className={`${styles.button} ${styles.negate} ${styles.lightGrey}`}>±</div>
-        <div className={`${styles.button} ${styles.percent} ${styles.lightGrey}`}>%</div>
-        <div className={`${styles.button} ${styles.divide} ${styles.yellow}`}>÷</div>
-        <div className={`${styles.button} ${styles.seven}`}>7</div>
-        <div className={`${styles.button} ${styles.eight}`}>8</div>
-        <div className={`${styles.button} ${styles.nine}`}>9</div>
-        <div className={`${styles.button} ${styles.times} ${styles.yellow}`}>×</div>
-        <div className={`${styles.button} ${styles.four}`}>4</div>
-        <div className={`${styles.button} ${styles.five}`}>5</div>
-        <div className={`${styles.button} ${styles.six}`}>6</div>
-        <div className={`${styles.button} ${styles.minus} ${styles.yellow}`}>-</div>
-        <div className={`${styles.button} ${styles.one}`}>1</div>
-        <div className={`${styles.button} ${styles.two}`}>2</div>
-        <div className={`${styles.button} ${styles.three}`}>3</div>
-        <div className={`${styles.button} ${styles.plus} ${styles.yellow}`}>+</div>
-        <div className={`${styles.button} ${styles.zero}`}>0</div>
-        <div className={`${styles.button} ${styles.dot}`}>.</div>
-        <div className={`${styles.button} ${styles.equal} ${styles.yellow}`}>=</div>
+        <div className={`${styles.screen}`}>{hasError ? "Error" : tokens.length === 0 ? 0 : tokens.join("")}</div>
+        <FunctionButton
+          className={`${styles.button} ${styles.ac} ${styles.lightGrey}`}
+          callback={() => {
+            setHasError(false);
+            setTokens([]);
+          }}
+        >
+          AC
+        </FunctionButton>
+        <FunctionButton
+          className={`${styles.button} ${styles.negate} ${styles.lightGrey}`}
+          callback={() => {
+            if (!hasError && tokens.length !== 0) {
+              setTokens([-eval(sanitizeOperationString(tokens.join("")))]);
+            }
+          }}
+        >
+          ±
+        </FunctionButton>
+        <FunctionButton
+          className={`${styles.button} ${styles.percent} ${styles.lightGrey}`}
+          callback={() => {
+            if (!hasError && tokens.length !== 0) {
+              setTokens([eval(sanitizeOperationString(tokens.join(""))) / 100]);
+            }
+          }}
+        >
+          %
+        </FunctionButton>
+        <ArithmeticButton className={`${styles.button} ${styles.Buttonide} ${styles.yellow}`}>÷</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.seven}`}>7</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.eight}`}>8</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.nine}`}>9</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.times} ${styles.yellow}`}>×</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.four}`}>4</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.five}`}>5</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.six}`}>6</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.minus} ${styles.yellow}`}>-</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.one}`}>1</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.two}`}>2</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.three}`}>3</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.plus} ${styles.yellow}`}>+</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.zero}`}>0</ArithmeticButton>
+        <ArithmeticButton className={`${styles.button} ${styles.dot}`}>.</ArithmeticButton>
+        <FunctionButton
+          className={`${styles.button} ${styles.equal} ${styles.yellow}`}
+          callback={() => {
+            if (tokens.length !== 0) {
+              const evalString = sanitizeOperationString(tokens.join(""));
+              const result = eval(evalString);
+              if (isNaN(result) || !isFinite(result)) {
+                setHasError(true);
+              } else {
+                setTokens([result]);
+              }
+            }
+          }}
+        >
+          =
+        </FunctionButton>
       </div>
     </Window>
   );
